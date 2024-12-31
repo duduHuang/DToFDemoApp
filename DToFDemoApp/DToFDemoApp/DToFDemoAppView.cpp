ad04d796edca9203927b5700e29629f5faaacb7d
tedHuang <ted.huang@luxvisions-inno.com>
2024-12-31 10:31:41 +0800

    initial.

---
+++ DToFDemoApp/DToFDemoApp/DToFDemoAppView.cpp
@@ -0,0 +1,128 @@
+﻿
+// DToFDemoAppView.cpp: CDToFDemoAppView 類別的實作
+//
+
+#include "pch.h"
+#include "framework.h"
+// SHARED_HANDLERS 可以定義在實作預覽、縮圖和搜尋篩選條件處理常式的
+// ATL 專案中，並允許與該專案共用文件程式碼。
+#ifndef SHARED_HANDLERS
+#include "DToFDemoApp.h"
+#endif
+
+#include "DToFDemoAppDoc.h"
+#include "DToFDemoAppView.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CDToFDemoAppView
+
+IMPLEMENT_DYNCREATE(CDToFDemoAppView, CView)
+
+BEGIN_MESSAGE_MAP(CDToFDemoAppView, CView)
+	// 標準列印命令
+	ON_COMMAND(ID_FILE_PRINT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_DIRECT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_PREVIEW, &CDToFDemoAppView::OnFilePrintPreview)
+	ON_WM_CONTEXTMENU()
+	ON_WM_RBUTTONUP()
+END_MESSAGE_MAP()
+
+// CDToFDemoAppView 建構/解構
+
+CDToFDemoAppView::CDToFDemoAppView() noexcept
+{
+	// TODO: 在此加入建構程式碼
+
+}
+
+CDToFDemoAppView::~CDToFDemoAppView()
+{
+}
+
+BOOL CDToFDemoAppView::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: 在此經由修改 CREATESTRUCT cs 
+	// 達到修改視窗類別或樣式的目的
+
+	return CView::PreCreateWindow(cs);
+}
+
+// CDToFDemoAppView 繪圖
+
+void CDToFDemoAppView::OnDraw(CDC* /*pDC*/)
+{
+	CDToFDemoAppDoc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+	if (!pDoc)
+		return;
+
+	// TODO: 在此加入原生資料的描繪程式碼
+}
+
+
+// CDToFDemoAppView 列印
+
+
+void CDToFDemoAppView::OnFilePrintPreview()
+{
+#ifndef SHARED_HANDLERS
+	AFXPrintPreview(this);
+#endif
+}
+
+BOOL CDToFDemoAppView::OnPreparePrinting(CPrintInfo* pInfo)
+{
+	// 預設的準備列印程式碼
+	return DoPreparePrinting(pInfo);
+}
+
+void CDToFDemoAppView::OnBeginPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 加入列印前額外的初始設定
+}
+
+void CDToFDemoAppView::OnEndPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 加入列印後的清除程式碼
+}
+
+void CDToFDemoAppView::OnRButtonUp(UINT /* nFlags */, CPoint point)
+{
+	ClientToScreen(&point);
+	OnContextMenu(this, point);
+}
+
+void CDToFDemoAppView::OnContextMenu(CWnd* /* pWnd */, CPoint point)
+{
+#ifndef SHARED_HANDLERS
+	theApp.GetContextMenuManager()->ShowPopupMenu(IDR_POPUP_EDIT, point.x, point.y, this, TRUE);
+#endif
+}
+
+
+// CDToFDemoAppView 診斷
+
+#ifdef _DEBUG
+void CDToFDemoAppView::AssertValid() const
+{
+	CView::AssertValid();
+}
+
+void CDToFDemoAppView::Dump(CDumpContext& dc) const
+{
+	CView::Dump(dc);
+}
+
+CDToFDemoAppDoc* CDToFDemoAppView::GetDocument() const // 內嵌非偵錯版本
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CDToFDemoAppDoc)));
+	return (CDToFDemoAppDoc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// CDToFDemoAppView 訊息處理常式
---
+++ DToFDemoApp/DToFDemoApp/DToFDemoAppView.h
@@ -0,0 +1,52 @@
+﻿
+// DToFDemoAppView.h: CDToFDemoAppView 類別的介面
+//
+
+#pragma once
+
+
+class CDToFDemoAppView : public CView
+{
+protected: // 僅從序列化建立
+	CDToFDemoAppView() noexcept;
+	DECLARE_DYNCREATE(CDToFDemoAppView)
+
+// 屬性
+public:
+	CDToFDemoAppDoc* GetDocument() const;
+
+// 作業
+public:
+
+// 覆寫
+public:
+	virtual void OnDraw(CDC* pDC);  // 覆寫以描繪此檢視
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+protected:
+	virtual BOOL OnPreparePrinting(CPrintInfo* pInfo);
+	virtual void OnBeginPrinting(CDC* pDC, CPrintInfo* pInfo);
+	virtual void OnEndPrinting(CDC* pDC, CPrintInfo* pInfo);
+
+// 程式碼實作
+public:
+	virtual ~CDToFDemoAppView();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+
+// 產生的訊息對應函式
+protected:
+	afx_msg void OnFilePrintPreview();
+	afx_msg void OnRButtonUp(UINT nFlags, CPoint point);
+	afx_msg void OnContextMenu(CWnd* pWnd, CPoint point);
+	DECLARE_MESSAGE_MAP()
+};
+
+#ifndef _DEBUG  // 對 DToFDemoAppView.cpp 中的版本進行偵錯
+inline CDToFDemoAppDoc* CDToFDemoAppView::GetDocument() const
+   { return reinterpret_cast<CDToFDemoAppDoc*>(m_pDocument); }
+#endif
+
